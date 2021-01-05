f099cb97c072a5adefd4f51e429620f290a0d945
John Tuckner <tuckner329@gmail.com>
2021-01-05 13:43:49 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Tines Connect
+
+Tines Connect is a container for [Ngrok](https://ngrok.com/) which can help provide access to resources that would generally not be internet accessible.
+
+## Install
+
+1. Create an Ngrok account. Find your `authtoken` value.
+
+2. Create a Global Resource in Tines and identify the ID number of the resource.
+
+3. Fill out `environment.txt` and `ngrok.yml`.
+
+4. Build the container.
+
+```
+docker build . --tag tines-connect
+```
+
+5. Run the container.
+
+```
+docker run --env-file environment.txt --name tines-connect tines-connect 
+```
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+
+set -x
+
+# Run Ngrok in background
+nohup ngrok start -log=stdout -config=/home/ngrok/.ngrok2/ngrok.yml --all &
+
+# Wait for Ngrok to register and then get dynamic address
+sleep 2
+URL=`curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url'`
+
+# Update Tines Agent with URL
+echo "Updating Tines Global Resource to $URL"
+curl -s -X PUT $TINES_URL/api/v1/global_resources/$TINES_GR_ID -H 'content-type: application/json' -H "x-user-email: $TINES_EMAIL" -H "x-user-token: $TINES_TOKEN" -d "{\"name\": \"tines_connect\", \"value_type\": \"text\", \"value\": \"$URL\"}"
+
+# Monitor background Ngrok service
+while sleep 10; do
+  ps aux |grep ngrok |grep -q -v grep
+  PROCESS_1_STATUS=$?
+  # If the greps above find anything, they exit with 0 status
+  # If they are not both 0, then something is wrong
+  if [ $PROCESS_1_STATUS -ne 0 ]; then
+    echo "Ngrok has exited so I will too."
+    exit 1
+  else
+    echo "Ngrok is still running."
+  fi
+done
