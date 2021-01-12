f019d52cf5658217477490f53a32c0d1024b4d5a
tuckner <tuckner329@gmail.com>
2021-01-11 22:25:26 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,10 @@
 
 set -x
 
+# Calculate Base64 Credential for GR
 AUTH=`echo "$AUTH_USERNAME:$AUTH_PASSWORD" | base64`
 
+# Set Ngrok config
 sed -i "s/auth:.*/auth: \"$AUTH_USERNAME:$AUTH_PASSWORD\"/" /home/ngrok/.ngrok2/ngrok.yml
 sed -i "s/authtoken:.*/authtoken: $NGROK_AUTHTOKEN/" /home/ngrok/.ngrok2/ngrok.yml
 sed -i "s/    addr:.*/    addr: $NGROK_ADDR/" /home/ngrok/.ngrok2/ngrok.yml
